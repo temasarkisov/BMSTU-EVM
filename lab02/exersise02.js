1b14690f33cfc4f977b45f09e3b27fde040ce1fc
tematematematema <a.sarkisov.s@yandex.ru>
2020-10-11 19:38:47 +0300

    Upload last version

--- lab02/exersise02.js
+++ lab02/exersise02.js
@@ -3,9 +3,9 @@ let math = require('./node_modules/mathjs');
 "use strict";
 
 class Triangle {
-    static length1 = 0;
-    static length2 = 0;
-    static length3 = 0;
+    length1 = 0;
+    length2 = 0;
+    length3 = 0;
 
     constructor(length1, length2, length3) {
         this.length1 = length1;
@@ -37,20 +37,19 @@ class Triangle {
         return math.sqrt(sp * (sp - this.length1) * (sp - this.length2) * (sp - this.length3)); 
     }
 
-    isRight() {
-        if (math.pow(this.length1, 2) + math.pow(this.length2, 2) === math.pow(this.length3, 2) ||
-            math.pow(this.length1, 2) + math.pow(this.length3, 2) === math.pow(math.powthis.length2) ||
-            math.pow(this.length3, 2) + math.pow(this.length2, 2) === math.pow(this.length1, 2)) {
-                return true;
-        }
-        return false;
+    isEqual(a, b, eps) {
+        return Math.abs(a - b) < eps;
     }
 
-
+    isRight(eps) {
+        return this.isEqual(this.length1 * this.length1, this.length2 * this.length2 + this.length3 * this.length3, eps) ||
+               this.isEqual(this.length2 * this.length2, this.length1 * this.length1 + this.length3 * this.length3, eps) ||
+               this.isEqual(this.length3 * this.length3, this.length2 * this.length2 + this.length1 * this.length1, eps);
+    }
 }
 
 var triangle = new Triangle(3, 4, 5);
 console.log(triangle.isExist());
 console.log(triangle.perimeterCalc());
 console.log(triangle.areaCalc());
-console.log(triangle.isRight());
+console.log(triangle.isRight(0.001));
